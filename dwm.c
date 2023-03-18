842f9f00b09528ab156fc612482e3271125f4035
Franco Comida <fcomida@users.sourceforge.net>
2023-03-19 00:43:34 +0100

    fix hide vacant tags

--- dwm.c
+++ dwm.c
@@ -567,48 +567,6 @@ unswallow(Client *c)
 	arrange(c->mon);
 }
 
-/* void */
-/* buttonpress(XEvent *e) */
-/* { */
-/* 	unsigned int i, x, click; */
-/* 	Arg arg = {0}; */
-/* 	Client *c; */
-/* 	Monitor *m; */
-/* 	XButtonPressedEvent *ev = &e->xbutton; */
-
-/* 	click = ClkRootWin; */
-/* 	/1* focus monitor if necessary *1/ */
-/* 	if ((m = wintomon(ev->window)) && m != selmon) { */
-/* 		unfocus(selmon->sel, 1); */
-/* 		selmon = m; */
-/* 		focus(NULL); */
-/* 	} */
-/* 	if (ev->window == selmon->barwin) { */
-/* 		i = x = 0; */
-/* 		do */
-/* 			x += TEXTW(tags[i]); */
-/* 		while (ev->x >= x && ++i < LENGTH(tags)); */
-/* 		if (i < LENGTH(tags)) { */
-/* 			click = ClkTagBar; */
-/* 			arg.ui = 1 << i; */
-/* 		} else if (ev->x < x + TEXTW(selmon->ltsymbol)) */
-/* 			click = ClkLtSymbol; */
-/* 		else if (ev->x > selmon->ww - (int)TEXTW(stext) - getsystraywidth()) */
-/* 			click = ClkStatusText; */
-/* 		else */
-/* 			click = ClkStatusText; */
-/* 	} else if ((c = wintoclient(ev->window))) { */
-/* 		focus(c); */
-/* 		restack(selmon); */
-/* 		XAllowEvents(dpy, ReplayPointer, CurrentTime); */
-/* 		click = ClkClientWin; */
-/* 	} */
-/* 	for (i = 0; i < LENGTH(buttons); i++) */
-/* 		if (click == buttons[i].click && buttons[i].func && buttons[i].button == ev->button */
-/* 		&& CLEANMASK(buttons[i].mask) == CLEANMASK(ev->state)) */
-/* 			buttons[i].func(click == ClkTagBar && buttons[i].arg.i == 0 ? &arg : &buttons[i].arg); */
-/* } */
-
 void
 buttonpress(XEvent *e)
 {
@@ -1034,13 +992,12 @@ drawbar(Monitor *m)
 	}
 	x = 0;
 	for (i = 0; i < LENGTH(tags); i++) {
+		/* Do not draw vacant tags */
+		if(!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
+			continue;
 		w = TEXTW(tags[i]);
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
-		if (occ & 1 << i)
-			drw_rect(drw, x + boxs, boxs, boxw, boxw,
-				m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
-				urg & 1 << i);
 		x += w;
 	}
 	w = blw = TEXTW(m->ltsymbol);
